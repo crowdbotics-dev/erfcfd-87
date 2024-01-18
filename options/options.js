2a55ebd623ef9bdc0fbccae41b5af2250b2ad10a
Crowdbotics <engineering@crowdbotics.com>
2024-01-18 11:13:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "erfcfd-87",
+  url: "https://erfcfd-87.botics.co",
+  api: "https://erfcfd-87.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
